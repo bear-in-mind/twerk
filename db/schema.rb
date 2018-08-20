856ae15dabadee69aaa0e59e04b549ab631d21a5
Claire Ziemendorf <claire.ziemendorf@gmail.com>
2018-08-20 16:19:15 +0200

    added review and experience model, controller, routes

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,29 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_20_130908) do
+ActiveRecord::Schema.define(version: 2018_08_20_140432) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "experiences", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.string "link_to_album"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_experiences_on_user_id"
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.text "content"
+    t.integer "rating"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_reviews_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email"
@@ -31,4 +50,6 @@ ActiveRecord::Schema.define(version: 2018_08_20_130908) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "experiences", "users"
+  add_foreign_key "reviews", "users"
 end
