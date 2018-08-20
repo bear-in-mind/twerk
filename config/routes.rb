ed2be6a2ba3cbd238b15a4681c9cedb279bdfea2
Louis Sommer <louis.sommer@gmail.com>
2018-08-20 16:50:37 +0200

    Delete routes.rb

--- config/routes.rb
+++
@@ -1,9 +0,0 @@
-Rails.application.routes.draw do
-  devise_for :users
-  resources :users do
-    resources :reviews, only: [ :new, :create]
-    resources :experiences, only: [ :new, :create, :update, :edit, :destroy]
-  end
-  root to: 'pages#home'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end
