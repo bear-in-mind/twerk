7b84cfaa7383250ed10d92340a1c5c4564521253
Julien Maisonnier <julien_maisonnier@yahoo.com>
2018-08-21 22:59:46 +0200

    job new view, draft. Missing sound file upload which needs to be added to Message class

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
   resources :reviews, only: [ :new, :create]
   resources :experiences, only: [ :new, :create, :update, :edit, :destroy]
   resources :jobs, only: [:new, :create, :index, :show]
+  resources :messages, only: [:new, :create, :index, :show]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
