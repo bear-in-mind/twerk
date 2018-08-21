4b82b988900166d0e524f8c1f5105880a5add1ae
Louis Sommer <louis.sommer@gmail.com>
2018-08-21 18:27:41 +0200

    Update message.rb

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,8 +1,7 @@
 class Message < ApplicationRecord
   belongs_to :user
   belongs_to :job
-  belongs_to :talent, :through => :job
-
+  
   validates :content, presence: true
   validates :user_id, presence: true
   validates :job_id, presence: true
