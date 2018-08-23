5b1f16edf6de86bfc59d8fc05547b02a98555112
Claire Ziemendorf <claire.ziemendorf@gmail.com>
2018-08-23 14:29:40 +0200

    edit button in profile page works again

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
 
-  resources :profiles, only: [:index, :show, :edit, :update] do
+  resources :profiles, only: [:index, :show, :edit] do
     resources :jobs, only: [:new, :create, :index, :show]
   end
+
+  patch "profiles/:id", to: "profiles#update", as: "profiles_update"
 
   resources :talents, only: [:new, :create, :edit, :update, :destroy]
   resources :reviews, only: [ :new, :create]
