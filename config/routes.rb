1ef98c761f340eefee1bc5cfee36a52f15674b38
Ziemendorf Claire <40497276+clairez16@users.noreply.github.com>
2018-08-23 16:28:42 +0200

    Updates routes again

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
 
-  resources :profiles, only: [:index, :show, :edit, :update] do
+  resources :profiles, only: [:index, :show, :edit] do
     resources :jobs, only: [:new, :create]
   end
+  patch "profiles/:id", to: "profiles#update", as: "profiles_update"
   resources :jobs, only: [:show, :index]
 
   resources :talents, only: [:new, :create, :edit, :update, :destroy]
